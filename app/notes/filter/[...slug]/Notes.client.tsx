544474bd44e5c5c8c3e351b13317183baef75681
Татьяна Татьяна <taty.bystrova@gail.com>
2025-07-01 22:45:29 +0300

    refresh Notes.client.tsx

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -64,7 +64,7 @@ type NotesHttpResponse = {
           </button>
           {isModalOpen && (
             <Modal onClose={closeModal}>
-              <NoteForm onClose={closeModal}/>
+              <NoteForm onClose={closeModal} />
             </Modal>
           )}
         </div>
